e86a958e63bac848351759c63aec4d38c5952664
yzz <1277745281@qq.com>
2018-01-24 11:40:05 +0800

    -

--- code/index.js
+++ code/index.js
@@ -1,2 +1,17 @@
-var express = require("express");
-debugger;
+// process.on('exit',function(code){
+//     console.log("退出代码为%d",code)
+// })
+// process.exit(1)
+//console.log(process.getgroups())
+
+// process.on("signal",function(s){
+//     console.log("signal",s);
+// })
+// process.kill(process.pid,"SIGINT")
+// console.log(process.memoryUsage())
+// process.nextTick(function(){
+//     console.log(56)
+// })
+setTimeout(() => {
+    console.log(process.uptime())
+}, 2000);
